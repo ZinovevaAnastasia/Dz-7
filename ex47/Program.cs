92f2485c5be1d72b2700091923fba0a67097677c
Nasty <zinovevaai@mail.ru>
2023-09-19 12:36:17 +0300

    dz7 ex47

---
+++ ex47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+double[,] CreateMatrixRndDouble(int rows, int columns, int min, int max)
+{
+  double[,] matrix = new double[rows, columns]; 
+  Random rnd = new Random();
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+     matrix[i, j] = rnd.NextDouble() * (max - min) + min;
+    }
+  }
+   return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j], 7:F2}");
+    }
+    Console.WriteLine();
+  }
+}
+
+double[,] array2d = CreateMatrixRndDouble(3, 4, -100, 100);
+PrintMatrix(array2d);
